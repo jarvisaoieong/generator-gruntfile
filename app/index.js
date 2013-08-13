34b682afaf4ef31b6e1a4d3962a64f52afe475b5
jarvis <kinua1230@gmail.com>
2013-08-13 22:17:18 +0800

    use js develop instead of coffee

--- app/index.js
+++ app/index.js
@@ -1,69 +1,61 @@
-var Generator, exports, path, yeoman,
-  __hasProp = {}.hasOwnProperty,
-  __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; };
+'use strict';
 
-yeoman = require('yeoman-generator');
+var path = require('path');
+var util = require('util');
+var yeoman = require('yeoman-generator');
 
-path = require('path');
-
-exports = module.exports = Generator = (function(_super) {
-  __extends(Generator, _super);
-
-  function Generator(args, options, config) {
-    Generator.__super__.constructor.apply(this, arguments);
-    this.basename = path.basename(options.env.cwd);
-    this.on('end', function() {
-      return this.installDependencies({
-        skipInstall: options['skip-install']
-      });
+exports = module.exports = function(args, options) {
+  yeoman.generators.Base.apply(this, arguments);
+  this.basename = path.basename(options.env.cwd);
+  this.on('end', function() {
+    this.installDependencies({
+      skipInstall: options['skip-install']
     });
-  }
+  });
+}
 
-  Generator.prototype.askFor = function() {
-    var done,
-      _this = this;
-    done = this.async();
-    console.log(this.yeoman);
-    return this.prompt([
-      {
-        name: 'name',
-        message: 'name',
-        "default": this.basename
-      }, {
-        name: 'description',
-        message: 'Description',
-        "default": ''
-      }, {
-        name: 'version',
-        message: 'Version',
-        "default": '0.0.0'
-      }, {
-        name: 'env',
-        type: 'list',
-        message: 'Target for',
-        choices: ['node', 'bower']
-      }
-    ], function(props) {
-      _this._.extend(_this, props);
-      return done();
-    });
-  };
+util.inherits(exports, yeoman.generators.Base);
 
-  Generator.prototype.copyRootFile = function() {
-    this.template('_README.md', 'README.md');
-    this.directory('root', '.');
-    this.template("" + this.env + "/_package.json", 'package.json');
-    this.template("" + this.env + "/_Gruntfile.coffee", 'Gruntfile.coffee');
-    if (this.env === 'bower') {
-      return this.template("" + this.env + "/_bower.json", 'bower.json');
+exports.prototype.askFor = function() {
+  var done = this.async();
+  console.log(this.yeoman);
+
+  this.prompt([
+    {
+      name: 'name',
+      message: 'name',
+      "default": this.basename
+    }, {
+      name: 'description',
+      message: 'Description',
+      "default": ''
+    }, {
+      name: 'version',
+      message: 'Version',
+      "default": '0.0.0'
+    }, {
+      name: 'env',
+      type: 'list',
+      message: 'Target for',
+      choices: ['node', 'bower']
     }
-  };
+  ], function(props) {
+    this._.extend(this, props);
+    done();
+  }.bind(this));
+};
 
-  Generator.prototype.writeSrcFile = function() {
-    this.mkdir('src');
-    return this.write("src/" + this.name + ".coffee", '');
-  };
+exports.prototype.copyRootFile = function() {
+  this.template('_README.md', 'README.md');
+  this.directory('root', '.');
+  this.template("" + this.env + "/_package.json", 'package.json');
+  this.template("" + this.env + "/_Gruntfile.coffee", 'Gruntfile.coffee');
+  if (this.env === 'bower') {
+    this.template("" + this.env + "/_bower.json", 'bower.json');
+  }
+};
 
-  return Generator;
-
-})(yeoman.generators.Base);
+exports.prototype.writeSrcFile = function() {
+  this.mkdir('src');
+  this.write("src/" + this.name + ".coffee", '');
+};
